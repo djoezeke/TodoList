60fc31ed906f2b777c80e9e820b56a070a3dd47b
Sackey Ezekiel Etrue <sackeyetrue@gmail.com>
2025-09-16 07:40:42 +0000

    [FIX] Vercel : build.sh and staticfiles errors

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+echo "Building the project..."
+
+# Install Python dependencies
+echo "Installing Python dependencies from requirements.txt..."
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
+
+# Run Django migrations
+# echo "Running Django migrations..."
+# python3 manage.py makemigrations --noinput
+# python3 manage.py migrate --noinput
+
+# Collect static files
+echo "Collecting static files..."
+python3 manage.py collectstatic --noinput --clear
+
+echo "Build process completed successfully."
